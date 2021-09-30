753097668221e5ca9cf8f3831633609f8772dab2
P22inolan <p22inolan@fairfieldprep.org>
2021-09-30 10:29:57 -0400

    Submit 9/30

--- hw3-1-3.py
+++ hw3-1-3.py
@@ -2,9 +2,13 @@ x = input("Give me a number. ")
 
 if int(x) % 2 == 0:
     print("Your number is a multiple of 2.")
+else:
+    print("Your number is not a mulitple of 2.")
 if int(x) % 3 == 0:
     print("Your number is a multiple of 3.")
+else:
+    print("Your number is not a mulitple of 3.")
 if int(x) % 5 == 0:
     print("Your number is a multiple of 5.")
 else:
-    print("Your number is not a mulitple of 2, 3, or 5.")
+    print("Your number is not a mulitple of 5.")
